d15c9a930c5c402ea7138edd0d052ae43faa72d7
yon3030 <jason1025yon@gmail.com>
2024-11-23 01:48:15 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# NextJS Landing Page Template 
+
+This is a Landing Page template for [Next.js](https://nextjs.org/) 14 App Router. 
+
+## Built Using and Initialised with ⚒️
+- Tailwind CSS
+- [Shadcn](https://ui.shadcn.com/) Components + Themes
+- Custom Components and Layouts that are fully responsive in both Mobile and Desktop View
+- Vercel's [Geist Font](https://vercel.com/font)
+
+## Preview
+![Preview](https://github.com/moinbukhari/NextJS-landing-page-template/blob/main/landing_page_preview.jpg)
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
---
+++ app/page.tsx
@@ -0,0 +1,328 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+
+import Link from "next/link";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+
+      {/* Hero */}
+      <MaxWidthWrapper className="mt-10 flex flex-col items-center justify-center text-center sm:mt-12">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+          <p className="text-sm font-semibold text-gray-700">
+            Try Convo for Free
+          </p>
+        </div>
+
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          <span className="text-blue-600">Speech</span> Focused Language
+          Learning
+        </h1>
+
+        <p className="mt-5 max-w-prose text-lg text-zinc-700 sm:text-2xl">
+          Convo helps you improve your spoken fluency with the help of an AI
+          tutor.
+        </p>
+
+        <Link
+          className={cn(
+            buttonVariants({
+              size: "lg",
+              className: "mt-5",
+            }),
+            "text-lg",
+          )}
+          href={"/dashboard"}
+        >
+          Start Speaking Now
+        </Link>
+      </MaxWidthWrapper>
+
+      {/* Value Prop */}
+      <div>
+        <div className="relative isolate">
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#0a95ff] to-[#95f2fa] opacity-30 sm:left-[calc(50%-20rem)] sm:w-[72.1875rem] sm:translate-y-8"
+            />
+          </div>
+
+          <div>
+            <div className="mx-auto flex max-w-6xl justify-center px-6 lg:px-8">
+              <div className="mt-8 flow-root sm:mt-16">
+                <div className="-m-2 w-fit rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10  lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <Image
+                    src="/dashboard-preview.png"
+                    alt="product preview"
+                    width={955}
+                    height={808}
+                    quality={100}
+                    className="rounded-md bg-special p-2 shadow-2xl ring-1 ring-gray-900/10 md:p-8"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative right-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] translate-x-1/3 rotate-[30deg] bg-gradient-to-tr from-[#0a95ff] to-[#95f2fa] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem] sm:translate-y-8"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Features */}
+      <MaxWidthWrapper>
+        <div className="mx-auto mt-20 flex max-w-5xl flex-col gap-20 sm:mt-40 sm:gap-40 ">
+          {/* Intro */}
+          <div>
+            <div className="mb-6 px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl sm:text-center">
+                <h2 className="mt-2 text-4xl font-bold text-gray-900 sm:text-5xl">
+                  Start your first session in seconds
+                </h2>
+                <p className="mt-4 text-lg text-gray-600">
+                  Improving your spoken fluency has never been easier than with
+                  Convo.
+                </p>
+              </div>
+            </div>
+            {/* steps */}
+
+            <ol className="my-2 space-y-4 pt-2 md:flex md:space-x-6 md:space-y-0 md:px-8">
+              <li className="md:flex-1">
+                <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+                  <span className="text-sm font-medium text-blue-600">
+                    Step 1
+                  </span>
+                  <span className="text-xl font-semibold">
+                    Sign up for a Free Account
+                  </span>
+                  {/* <span className="mt-2 text-zinc-700">
+                Either starting out with a free plan or choose our{" "}
+                <Link
+                  href="/pricing"
+                  className="text-blue-700 underline underline-offset-2"
+                >
+                  pro plan
+                </Link>
+                .
+              </span> */}
+                </div>
+              </li>
+              <li className="md:flex-1">
+                <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+                  <span className="text-sm font-medium text-blue-600">
+                    Step 2
+                  </span>
+                  <span className="text-xl font-semibold">
+                    Choose a <span className="text-blue-600">Language</span> &{" "}
+                    <span className="text-blue-600">Scenario</span>
+                  </span>
+                </div>
+              </li>
+              <li className="md:flex-1">
+                <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+                  <span className="text-sm font-medium text-blue-600">
+                    Step 3
+                  </span>
+                  <span className="text-xl font-semibold">
+                    Start Your Speaking Session
+                  </span>
+                  {/* <span className="mt-2 text-zinc-700">
+                
+              </span> */}
+                </div>
+              </li>
+            </ol>
+          </div>
+
+          {/* Scenarios */}
+          <div>
+            <div className="mb-6 px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl sm:text-center">
+                <h2 className="mt-2 text-4xl font-bold text-gray-900 sm:text-5xl">
+                  Practice in Real Life Scenarios
+                </h2>
+                <p className="mt-4 text-lg text-gray-600">
+                  Get help if you make a mistake and guidance on saying
+                  something in your target language.
+                </p>
+              </div>
+            </div>
+            {/* steps */}
+
+            <div className="flex items-center justify-center">
+              <div className="flex flex-col gap-6 md:flex-row">
+                <Card className="flex flex-col items-center justify-center gap-2 p-6 md:flex-1">
+                  <CardTitle>At the Cafe</CardTitle>
+                  <CardDescription className="mb-3 text-center">
+                    Learn how to order drinks and snacks
+                  </CardDescription>
+                  <Image
+                    src="/coffee.webp"
+                    alt="cafe scenario"
+                    width={128}
+                    height={128}
+                    quality={100}
+                  />
+                </Card>
+                <Card className="flex flex-col items-center justify-center gap-2 p-6 md:flex-1">
+                  <CardTitle>Getting A Taxi</CardTitle>
+                  <CardDescription className="mb-3 text-center">
+                    Learn how to navigate and get directions
+                  </CardDescription>
+                  <Image
+                    src="/taxi.webp"
+                    alt="cafe scenario"
+                    width={128}
+                    height={128}
+                    quality={100}
+                  />
+                </Card>
+
+                <Card className="flex flex-col items-center justify-center gap-2 p-6 md:flex-1">
+                  <CardTitle>Social Activities</CardTitle>
+                  <CardDescription className="mb-3 text-center">
+                    Learn how to make plans and catch up with friends
+                  </CardDescription>
+                  <Image
+                    src="/schedule.webp"
+                    alt="cafe scenario"
+                    width={128}
+                    height={128}
+                    quality={100}
+                  />
+                </Card>
+              </div>
+            </div>
+            <div className="mb-6 px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl text-center">
+                <p className="mt-4 text-lg text-gray-600">and many more...</p>
+              </div>
+            </div>
+          </div>
+
+          {/* Powered By */}
+          <div>
+            <div className="mb-6 px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl sm:text-center">
+                <h2 className="mt-2 text-4xl font-bold text-gray-900 sm:text-5xl">
+                  Powered by
+                </h2>
+                <p className="mt-4 text-lg text-gray-600">
+                  These are the technologies behind the scene that make Convo
+                  possible
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-center">
+              <div className="flex flex-col gap-6 md:flex-row">
+                <Card className="flex flex-col items-center justify-center gap-2 p-6 md:flex-1">
+                  <CardTitle>Whisper AI</CardTitle>
+                  <CardDescription className="mb-3 text-center">
+                    Speech to text
+                  </CardDescription>
+                  <Image
+                    src="/mic.webp"
+                    alt="cafe scenario"
+                    width={128}
+                    height={128}
+                    quality={100}
+                    className="rounded-xl"
+                  />
+                </Card>
+                <Card className="flex flex-col items-center justify-center gap-2 p-6 md:flex-1">
+                  <CardTitle>GPT 4</CardTitle>
+                  <CardDescription className="mb-3 text-center">
+                    Comprehensive prompts and responses
+                  </CardDescription>
+                  <Image
+                    src="/openai.webp"
+                    alt="cafe scenario"
+                    width={128}
+                    height={128}
+                    quality={100}
+                    className="rounded-xl"
+                  />
+                </Card>
+
+                <Card className="flex flex-col items-center justify-center gap-2 p-6 md:flex-1">
+                  <CardTitle>WebSpeech</CardTitle>
+                  <CardDescription className="mb-3 text-center">
+                    Text to speech
+                  </CardDescription>
+                  <Image
+                    src="/speaker.webp"
+                    alt="cafe scenario"
+                    width={128}
+                    height={128}
+                    quality={100}
+                    className="rounded-xl"
+                  />
+                </Card>
+              </div>
+            </div>
+          </div>
+
+          {/* Feedback */}
+          <div>
+            <div className="mb-6 px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl sm:text-center">
+                <h2 className="mt-2 text-4xl font-bold text-gray-900 sm:text-5xl">
+                  Feedback
+                </h2>
+                <p className="mt-4 text-lg text-gray-600">
+                  What are people saying about Convo
+                </p>
+              </div>
+            </div>
+            {/* steps */}
+
+            <div>
+              <div className="mx-auto flex max-w-6xl justify-center px-6 lg:px-8">
+                <div className="flow-root">
+                  <div className="-m-2 w-fit rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10  lg:-m-4 lg:rounded-2xl lg:p-4">
+                    <Image
+                      width={2556}
+                      height={1436}
+                      quality={100}
+                      src="/tweet_collage.png"
+                      alt="Header image"
+                      className="rounded-md bg-white p-2 shadow-2xl ring-1 ring-gray-900/10 md:p-8"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+
+      <Footer />
+    </>
+  );
+}
